543ab54128be8d21e0896e9ef7d03095098c2c5c
Hamid00777845 <salomatlik87@gmail.com>
2025-07-03 15:41:14 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let meva =prompt("Ismingizni kiriting")
+let parol =prompt("SpacePassword kiriting")
+let nom =prompt("Space parol kriting")
+
+const student = {
+    name:meva,
+    id:parol,
+    pasword:nom,
+}
+console.log(student);
+
